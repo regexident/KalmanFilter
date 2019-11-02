a6e54bd62ed8b7dd2e9742ccb12b4978491886e5
Vincent Esche <regexident@gmail.com>
2019-11-02 15:45:36 +0100

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "KalmanFilter",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "KalmanFilter",
             targets: [
@@ -17,11 +16,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/regexident/BayesFilter", .branch("master")),
         .package(url: "https://github.com/regexident/StateSpaceModel", .branch("master")),
-        .package(url: "https://github.com/jounce/Surge", .branch("master")),
+        .package(url: "https://github.com/jounce/Surge", from: "2.3.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "KalmanFilter",
             dependencies: [
